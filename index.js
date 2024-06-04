b1bc0e4db8cba475823be5360aefce1d8b58ca6a
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-04 11:17:00 +0200

    render items

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const squareDigits = (num) => {
-  // Convert the number to a string
-  const numStr = num.toString();
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-  // Initialize an empty string to hold the result
-  let result = "";
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  // 2. Call the renderLeads() function
+  renderLeads();
+});
 
-  // Loop through each character in the string
-  for (let char of numStr) {
-    // Convert the character back to a number and square it
-    let square = Number(char) ** 2;
-
-    // Convert the squared number back to a string and add it to the result
-    result += square.toString();
+// 1. Wrap the code below in a renderLeads() function
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
   }
-
-  // Convert the result string back to a number and return it
-  return Number(result);
-};
-
-console.log(squareDigits(2112)); //  4114
-console.log(squareDigits(3212)); //  9414
-console.log(squareDigits(9159)); //  8112581
+  ulEl.innerHTML = listItems;
+}
